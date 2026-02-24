b782796a551762f7cde314d5e2c64f809d5186e8
Tanishk <tanishk2300@gmail.com>
2026-02-25 00:35:43 +0530

    adding a project

---
+++ 2_Section/RepetitionControlStatements.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int count=0;
+//while loop 
+    while (count<10)
+    {
+        cout<<"hello"<<endl;
+        count++;
+    }
+// do while loop 
+    int counter2=100;
+    do
+    {
+        cout<<"counter2 :"<<counter2<<endl;
+        counter2++;
+    } while (counter2<10);
+// for loop 
+    for (int i=0;i<10;i++)
+    {
+        cout<<"number:"<<i<<endl;
+    }
+
+//sentinel-controlled repetition 
+    int input;
+    cout<<"enter a non-negative integer :"<<endl;
+    while(input>=0){
+        cout<<"you entered :"<<input<<endl;
+        cout<<"enter another number or negative to quit :"<<endl;
+        cin>>input;
+
+
+    }
+    return 0;
+};
