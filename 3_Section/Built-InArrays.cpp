7b56b5feff0a353dd5409ed03ab4216668e626cc
Tanishk <tanishk2300@gmail.com>
2026-02-27 01:09:42 +0530

    section-3

--- 3_Section/Built-InArrays.cpp
+++ 3_Section/Built-InArrays.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    const int array_size=5;
+    int myarray[array_size];
 
+    myarray[0]=12;
+    myarray[1]=21;
+    myarray[2]=9;
+    myarray[3]=90;
+    myarray[4]=32;
+
+    for (int i=0;i<array_size;i++){
+        cout<<myarray[i]<<endl;
+
+    }
     return 0;
 }
