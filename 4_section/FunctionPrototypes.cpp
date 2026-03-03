f4fdf79cc48dfe7c3b9ba47b99c1eb8439ee7e1f
Tanishk <tanishk2300@gmail.com>
2026-03-04 01:48:31 +0530

    new file

---
+++ 4_section/FunctionPrototypes.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+void printMyName(){
+    int name;
+    cout <<"my name is tanishk."<<endl;//we write the statement here .
+}
+int main (){
+    printMyName();// here we call the void function .
+    return 0;
+}
