7e116e2f0d0a44b2c7504302f68eebf78ad6821f
Tanishk <tanishk2300@gmail.com>
2026-03-05 23:50:32 +0530

    parameter passing

---
+++ 4_section/ParameterPassing.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void valuechange(int& somenum);
+int main (){
+    int mynumber;
+    mynumber=100;
+    cout<<"before changing number:"<<mynumber<<endl;
+
+    valuechange(mynumber);
+    cout<<"after changing the value:"<<mynumber<<endl;
+
+    return 0;
+}
+void valuechange(int& somenum){ // '&' this is use to change the value of main .
+    somenum=120;
+}
